3afef0c2fae4877d4db860522b13932b07d17c2b
pierzbarry <pmbarry@asu.edu>
2019-10-22 16:41:13 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i = 1; i < 10; i++){
+            for(int a = 0; a < i; a++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
